4d4c9fbbda30e7055a84fb5ef478566614efb4a0
Jeong Jihun <doksakim7@naver.com>
2026-05-05 21:03:24 +0900

    feat: BaseEntity, SoftDeleteEntity, JpaAuditingConfig 완성

---
+++ src/main/java/kr/spartaclub/coffeeproject/common/entity/SoftDeleteEntity.java
@@ -0,0 +1,29 @@
+package kr.spartaclub.coffeeproject.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class SoftDeleteEntity extends BaseEntity {
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public void delete() {
+        if (this.deletedAt != null) {
+            return;
+        }
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public boolean isDeleted() {
+        return this.deletedAt != null;
+    }
+}
